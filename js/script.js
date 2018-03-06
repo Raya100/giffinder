3f0d119be1ca809bbb00e3f5700321a58684ef31
Jaraya Wright <jaraya.wright@greenschoolbrooklyn.com>
2018-03-06 23:27:34 +0000

    started new project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/*global $*/
+
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
@@ -8,3 +10,23 @@ $(document).ready(function(){
   
   
 });
+
+function giphyURLWithSearchTerm(searchTerm) {
+    // write a function that will return a url for the giphy API with
+    // the searchTerm provided in the parameters
+}
+
+function appendImageToBody(srcURL) {
+    // write a function that will append an <img> to the body with the
+    // URL provided in the parameters
+}
+function callGiphyAPIWithSearchTerm(searchTerm) {
+    $.ajax({
+      url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+$("body").append("<img src="+response.data[0].images.original.url+"/>");
+
+  },
+    }); 
+}
